2097aaad6c565d53b050159fa9a11b11f0c91b74
newtypeV2 <gianlibunao@gmail.com>
2019-05-24 11:02:50 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
-class Dog
+
+
+class Dog < ActiveRecord::Base
+
+    # attr_accessor :name, :breed, :id
+
+    # def update(name)
+    #     # binding.pry
+    # end
+    
+
+    # def self.find_by_name(name)
+    #     # binding.pry
+    #     Dog.find_by(name: name)
+    # end
+    
+
 end
